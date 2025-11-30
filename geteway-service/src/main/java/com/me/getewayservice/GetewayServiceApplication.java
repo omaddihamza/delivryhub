d44a6706a1f4a69bf9bf9b6d4646a8aa2be403e4
omaddihamza <hamza.omaddi@gmail.com>
2025-11-30 12:12:32 +0100

    first commit

---
+++ geteway-service/src/main/java/com/me/getewayservice/GetewayServiceApplication.java
@@ -0,0 +1,22 @@
+package com.me.getewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GetewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetewayServiceApplication.class, args);
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties) {
+      return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+	}
+
+}
