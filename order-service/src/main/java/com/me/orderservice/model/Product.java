38f8e0e4dd3a73cc45088939fbd486685c1a1480
omaddihamza <hamza.omaddi@gmail.com>
2025-12-16 12:39:09 +0100

    some change in order service

--- order-service/src/main/java/com/me/orderservice/model/Product.java
+++ order-service/src/main/java/com/me/orderservice/model/Product.java
@@ -1,17 +1,11 @@
 package com.me.orderservice.model;
 
-
 import lombok.*;
-
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @Builder
 @ToString
 public class Product {
     private Long id;
--- order-service/src/main/java/com/me/orderservice/reposirory/OrderRepository.java
+++ order-service/src/main/java/com/me/orderservice/reposirory/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.me.orderservice.reposirory;
 
 import com.me.orderservice.entities.Order;
-import com.me.orderservice.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
